a0167235dc121e87101fe8469b695b5ce83ceca3
Takeuchi <gyohunori1985@gmail.com>
2024-09-25 10:48:43 +0900

    トップ画面と詳細画面を作成

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
 
-export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.MICRO_CMS_IMAGE_DOMAIN,
+        port: '',
+        pathname: '/assets/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,67 @@
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import Image from 'next/image'
+import { client } from '../../libs/client'
+import { Post, PostsListResponse } from 'types'
+
+type Props = {
+  post: Post
+}
+
+const PostShow: NextPage<Props> = ({ post }: Props) => {
+  return (
+    <main>
+      <h1>{post.title}</h1>
+      <p>{post.publishedAt}</p>
+      <p>{post.category.name}</p>
+      {/* <p>{post.eyecatch.url}</p> */}
+      <Image
+        src={post.eyecatch.url}
+        alt={post.title}
+        width={post.eyecatch.width}
+        height={post.eyecatch.height}
+      />
+      <div>{post.content}</div>
+    </main>
+  )
+}
+
+type PathParams = {
+  id: string
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const id = ctx.params?.id as string
+  try {
+    const post: Post = await client.get({
+      endpoint: 'posts',
+      contentId: id,
+    })
+
+    return {
+      props: {
+        post: post,
+      },
+    }
+  } catch {
+    return {
+      notFound: true,
+    }
+  }
+}
+
+export const getStaticPaths: GetStaticPaths<PathParams> = async () => {
+  const data: PostsListResponse = await client.get({
+    endpoint: 'posts',
+  })
+  const posts = data.contents
+  const paths = posts.map((post) => {
+    return {
+      params: {
+        id: post.id,
+      },
+    }
+  })
+  return { paths, fallback: 'blocking' }
+}
+
+export default PostShow
